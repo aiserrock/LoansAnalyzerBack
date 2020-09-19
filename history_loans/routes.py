e4b528d7b77a866a5ce2c95519fd07bb894e5d93
aiserrock <aicserrock@gmail.com>
2020-09-19 12:02:08 +0100

    added new type RETURN_DEPT and RETURN_PROCENT

--- history_loans/routes.py
+++ history_loans/routes.py
@@ -26,6 +26,15 @@ async def _get_history_loan_row_id(id: str, loans_id: str):
         raise HTTPException(status_code=404, detail="row not found")
 
 
+async def _get_history_loan_row_with_loans_id(row_id: str):
+    _id = validate_object_id(row_id)
+    row = await db.history_loans.find_one({"_id": _id})
+    if row:
+        return fix_id(row)
+    else:
+        raise HTTPException(status_code=404, detail="row not found")
+
+
 # @history_loans_router.get("/{history_loan_row_id}", status_code=HTTP_200_OK)
 # async def get_history_loan_row_by_id(history_loan_row_id: str,
 #                                      current_user: UserResponse = Depends(get_current_active_user)):
@@ -41,7 +50,7 @@ async def get_all_history_loans_by_loans_id(loan_id: str, row_id: str = "", limi
     иначе если row_id не пусто, то возращает конкретную запись из истории выплат по данному займу
     """
     if row_id != "":
-        row = await _get_history_loan_row_id(row_id,loan_id)
+        row = await _get_history_loan_row_id(row_id, loan_id)
         return row
     else:
         history_loans_cursor = db.history_loans.find(
@@ -57,24 +66,32 @@ async def get_all_history_loans_by_loans_id(loan_id: str, row_id: str = "", limi
 async def update_client_by_id(history_loan_row_id: str, history_loan_row: HistoryLoansChange,
                               current_user: UserResponse = Depends(get_current_active_user)):
     # # valid
+    """
+        Valid status: DEPT, PROCENT, RETURN_DEPT, RETURN_PROCENT
+    """
     history_loan_row.amount = float(history_loan_row.amount)
     if history_loan_row.amount < 0:
         raise HTTPException(status_code=400, detail="amount must be non-negative ")
 
     try:
+        tmp = await _get_history_loan_row_with_loans_id(history_loan_row_id)
         history_loan_row.type = HistoryLoanType(history_loan_row.type)
     except:
         raise HTTPException(status_code=400,
                             detail="Invalid type name " + history_loan_row.type + ". Valid status: DEPT, PROCENT")
     if not isinstance(history_loan_row.date, datetime):
         raise HTTPException(status_code=400, detail="Invalid type datetime")
-    history_loan_roww = await _get_history_loan_row_id(history_loan_row_id)
+    history_loan_roww = await _get_history_loan_row_id(history_loan_row_id, tmp['loans_id'])
     loans_id = history_loan_roww["loans_id"]
     loan = await _get_loan(loans_id)
     loan_date = loan['issued_at']
     if history_loan_row.date.isocalendar() < loan_date.isocalendar():
         raise HTTPException(status_code=400, detail="Date pay must be older then date issued")
     #############################
+
+    if history_loan_row.type == HistoryLoanType.RETURN_DEPT or history_loan_row.type == HistoryLoanType.RETURN_PROCENT:
+        history_loan_row.amount = -history_loan_row.amount
+
     result = await db.history_loans.update_one({"_id": ObjectId(history_loan_row_id)}, {"$set": {
         "amount": history_loan_row.amount,
         "date": history_loan_row.date,
@@ -96,6 +113,9 @@ async def delete_history_loan_row_by_id(history_loan_row_id,
 @history_loans_router.post('/', status_code=HTTP_201_CREATED, response_model=HistoryLoansDB)
 async def create_history_loan_row(history_loan_row: HistoryLoansCreate,
                                   current_user: UserResponse = Depends(get_current_active_user)):
+    """
+    Valid status: DEPT, PROCENT, RETURN_DEPT, RETURN_PROCENT
+    """
     # valid
     history_loan_row.amount = float(history_loan_row.amount)
     if history_loan_row.amount < 0:
@@ -105,7 +125,7 @@ async def create_history_loan_row(history_loan_row: HistoryLoansCreate,
         history_loan_row.type = HistoryLoanType(history_loan_row.type)
     except:
         raise HTTPException(status_code=400,
-                            detail="Invalid type name " + history_loan_row.type + ". Valid status: DEPT, PROCENT")
+                            detail="Invalid type name " + history_loan_row.type + ". Valid status: DEPT, PROCENT, RETURN_DEPT, RETURN_PROCENT")
     if not isinstance(history_loan_row.date, datetime):
         raise HTTPException(status_code=400, detail="Invalid type datetime")
     loan = await _get_loan(history_loan_row.loans_id)
@@ -113,6 +133,10 @@ async def create_history_loan_row(history_loan_row: HistoryLoansCreate,
     if history_loan_row.date.isocalendar() < loan_date.isocalendar():
         raise HTTPException(status_code=400, detail="Date pay must be older then date issued")
     ######################
+
+    if history_loan_row.type == HistoryLoanType.RETURN_DEPT or history_loan_row.type == HistoryLoanType.RETURN_PROCENT:
+        history_loan_row.amount = -history_loan_row.amount
+
     result = await db.history_loans.insert_one(
         {
             "amount": history_loan_row.amount,
@@ -126,6 +150,3 @@ async def create_history_loan_row(history_loan_row: HistoryLoansCreate,
         return fix_id(row)
     else:
         raise HTTPException(status_code=500, detail="Payment hasn't been created")
-
-
-
